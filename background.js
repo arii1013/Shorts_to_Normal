43fbce35c1120efc08a913b9ef676dc6b129cd8e
arii1013 <sonrisauba@gmail.com>
2022-09-12 19:15:23 +0900

    On/Off 기능"만" 추가, 작동은 안함.

---
+++ background.js
@@ -0,0 +1,26 @@
+// 클릭 이벤트 리스너
+chrome.action.onClicked.addListener((tab) => {
+    chrome.storage.local.get("isEnabled", (res) => {
+        let isEnabled = !res.isEnabled;
+        let text = isEnabled ? "ON" : "OFF";
+
+        chrome.action.setBadgeText({ text: text })
+        chrome.storage.local.set({ "isEnabled": isEnabled });
+    });
+});
+
+// 새로고침 이벤트 리스너
+chrome.tabs.onActivated.addListener(() => {
+    chrome.storage.local.get("isEnabled", (res) => {
+        let isEnabled = res.isEnabled;
+        let text = isEnabled ? "ON" : "OFF";
+
+        chrome.action.setBadgeText({ text: text })
+        chrome.storage.local.set({ "isEnabled": isEnabled });
+    });
+});
+
+// 크롬 익스텐션 처음 설치했을 때
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ "isEnabled": true });
+});
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function test() {
-    var bodyText = window.location.toString().replace("/shorts/", "/watch?v=");
-    document.location.href = bodyText;
-}
+// function test() {
+//     var bodyText = window.location.toString().replace("/shorts/", "/watch?v=");
+//     document.location.href = bodyText;
+// }
 
-chrome.tabs.query({ active: true }, function (tabs) {
-    let tab = tabs[0];
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: test,
-        },
-        (injectionResults) => displaySearch(injectionResults[0].result)
-    );
-});
+// chrome.tabs.query({ active: true }, function (tabs) {
+//     let tab = tabs[0];
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         function: test,
+//         },
+//         // (injectionResults) => displaySearch(injectionResults[0].result)
+//     );
+// });
